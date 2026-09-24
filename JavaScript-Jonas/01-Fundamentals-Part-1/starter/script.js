7f5eed27b63a64cf001d0e6ef1fd7783164a11ed
Abner_SD <abnersoares639@gmail.com>
2026-09-24 11:57:46 -0300

    js 1

---
+++ JavaScript-Jonas/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+/*
+                Section 1
+  let js = 'amazing';
+      if (js === 'amazing') alert('JavaScript is FUN!');
+  console.log(40 + 8 + 23 - 11);
+*/
+
+/*              Section 2 */
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda'; //Declaring a variable
+
+console.log(firstName);
