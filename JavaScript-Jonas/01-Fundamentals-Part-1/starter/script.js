0e4a342575b31425daedbc0d6d606405c7ed34f9
Abner_SD <abnersoares639@gmail.com>
2026-09-25 10:28:42 -0300

    js 2

--- JavaScript-Jonas/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript-Jonas/01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,41 @@
 /*
-                Section 1
   let js = 'amazing';
       if (js === 'amazing') alert('JavaScript is FUN!');
   console.log(40 + 8 + 23 - 11);
 */
 
-/*              Section 2 */
 
+/* 
 console.log('Jonas');
 console.log(23);
 
 let firstName = 'Matilda'; //Declaring a variable
 
-console.log(firstName);
+console.log(firstName); */
+
+/*       Data Types
+
+let javascripIsFun;
+let valueChange = false;
+valueChange = 'YES'; //To change values you don't use 'let'
+
+console.log(typeof true);
+console.log(typeof javascripIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof valueChange);
+
+ */
+
+/*      let const and var 
+
+let age = 30;  //Variable that can be mutate
+age = 31;
+
+const birthYear = 1991; //Variable that can not be mutate
+
+var job = 'programmer'
+job = 'teacher'
+*/
+
+/*      Basic Operators */
